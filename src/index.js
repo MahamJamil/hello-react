d166739465b5ca4e4d2160b2028ebc8726010d9d
Maham Jamil Khan <mahamjk42@gmail.com>
2020-10-30 22:40:09 +0500

    using objects in JSX

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = 'Islamabad';
+let city = {
+    name: 'Islamabad',
+    country: 'Pakistan'
+};
 
-ReactDOM.render( < h1 > Hello from { city }! < /h1>,
+ReactDOM.render( < h1 > Hello from { city.name }, { city.country }! < /h1>,
     document.getElementById('root')
 );
