d3e5345a2d84d0a3cc3af3826a1043d0d571a1f6
Maham Jamil Khan <mahamjk42@gmail.com>
2020-11-15 20:01:09 +0500

    removed city array and called hello as self-closing JSX tag

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = {
-    name: 'Islamabad',
-    country: 'Pakistan'
-};
 
 function Hello() {
     return <h1 > Welcome to React! < /h1>
 }
 
-ReactDOM.render( < Hello > < /Hello>,
+ReactDOM.render( < Hello / > ,
     document.getElementById('root')
 );
