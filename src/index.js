3535e9eb31921b3e281ea924389b2ab1523a6512
Maham Jamil Khan <mahamjk42@gmail.com>
2020-11-15 20:42:44 +0500

    Added Lake component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function Lake(props) {
+    return ( <
+        h1 > { props.name } < /h1>
+    )
+}
+
 function App() {
     return <div > App < /div>
 }
-
-
 
 ReactDOM.render( < App / > ,
     document.getElementById('root')
