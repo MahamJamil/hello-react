b2aec6aef918f8766bfd32832b139db6abafa778
Maham Jamil Khan <mahamjk42@gmail.com>
2020-11-17 12:22:15 +0500

    added keys by using toString function on item list

--- src/index.js
+++ src/index.js
@@ -3,26 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const list = [1, 2, 3, 4, 5]
-const lakeList = [
-    { id: "1", name: "Echo", trailhead: "Lake" },
-    { id: "2", name: "Maud", trailhead: "Wrights" },
-    { id: "3", name: "Cascade", trailhead: "Bayview" }
-]
 
-function App({ lakes }) {
-    return ( < div > {
-            lakes.map(lake => ( <
-                div key = { lake.id } >
-                <
-                h2 > { lake.name } < /h2> <
-                p > Accessed by { lake.trailhead } < /p> < /
-                div >
-            ))
-        } <
-        /div>)
-    }
+function App({ items }) {
+    return ( <
+            ul > {
+                items.map((item => < li key = { item.toString() } > { item } < /li>))} < /
+                        ul >
+                    )
+                }
 
-    ReactDOM.render( < App lakes = { lakeList }
-        / > ,
-        document.getElementById('root')
-    );
+                ReactDOM.render( < App items = { list }
+                    / > ,
+                    document.getElementById('root')
+                );
