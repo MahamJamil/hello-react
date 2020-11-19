494268e9648e9b27faccb4503328e9b1a84e86ad
Maham Jamil Khan <mahamjk42@gmail.com>
2020-11-19 21:25:11 +0500

    Added GithubUser component

--- src/index.js
+++ src/index.js
@@ -2,37 +2,12 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function GithubUser() {
+
+}
+
 function App() {
-    const [val, setVal] = useState("");
-    const [val2, setVal2] = useState("");
 
-    useEffect(() => {
-        console.log(`field 1:
-        $ { val }`, [val])
-    })
-
-    useEffect(() => {
-        console.log(`field 2:
-        $ { val2 }`, [val, val2])
-    })
-
-    return ( <
-        >
-        <
-        label >
-        Favorite Phrase:
-        <
-        input value = { val }
-        onChange = { e => setVal(e.target.value) }
-        / > < /
-        label > <
-        br / >
-        <
-        label > Second Favorite Phrase: < input value = { val2 }
-        onChange = { e => setVal2(e.target.value) }
-        /> < /
-        label > < / >
-    )
 }
 
 ReactDOM.render( < App / > ,
