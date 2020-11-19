a259a409d6a7c6725f754c2171a94db5c7f81f32
Maham Jamil Khan <mahamjk42@gmail.com>
2020-11-19 20:12:54 +0500

    Added alert on state change

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 
 function CheckBox() {
     const [checked, setChecked] = useState(true)
+    alert(`checked: ${checked.toString()}`)
     return ( <
         >
         <
         input type = "checkbox"
         value = "checked"
         onChange = {
-            () => setChecked(checked => !checked) }
-        / > { checked ? "checked" : "not checked" } < /
-        >
+            () => setChecked(checked => !checked)
+        }
+        / > { checked ? "checked" : "not checked" } < / >
     )
 
 }
