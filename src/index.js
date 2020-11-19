90aa9f56f7ef4bc7f10449f54cdf1b58e49be9a4
Maham Jamil Khan <mahamjk42@gmail.com>
2020-11-19 19:58:13 +0500

    Added CheckBox component

--- src/index.js
+++ src/index.js
@@ -1,49 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App({ name }) {
-    const [year, setYear] = useState(2050)
-    const [manager, setManager] = useState("Alex")
-    const [status, setStatus] = useState("Open")
-    return ( <
-        >
-        <
-        div >
-        <
-        h1 > { year } < /h1> <
-        button onClick = {
-            () => setYear(year + 1)
-        } > New Year! < /button> < /
-        div > <
-        div >
-        <
-        h1 > Manager on Duty: { manager } < /h1>  <
-        button onClick = {
-            () => setManager("Rachel")
-        } > New Manager < /button> < /
-        div >
-        <
-        div >
-        <
-        h1 > Status: { status } < /h1>   <
-        button onClick = {
-            () => setStatus("Open")
-        } > Open < /button >  <
-        button onClick = {
-            () => setStatus("Back in 5 mins")
-        } > Back < /button > <
-        button onClick = {
-            () => setStatus("Closed")
-        } > Closed < /button> < /
-        div >
-        <
-        />
-    );
+function CheckBox() {
 
 
 }
 
-ReactDOM.render( < App / > ,
+ReactDOM.render( < CheckBox / > ,
     document.getElementById('root')
 );
